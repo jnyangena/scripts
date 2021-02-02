c4301017610ae48a752e1eb6551522e82959497f
jnyangena <jbngena@gmail.com>
2021-02-02 11:29:57 +0300

    This commit adds functionality to get user input for the number of folders and prefix for the folder names.

--- dir.sh
+++ dir.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/bash
 
-end=$1 #This variable takes the second argument on the command line as the number of folders to be created by the script
 
-for ((week=1;week<=end;week++)); #initiate a for loop that loops the number of times specified in the second argument
+echo "How many folders do you want?"
+#end=$1 #This variable takes the second argument on the command line as the number of folders to be created by the script
+
+read number
+
+echo "What is the prefix you want for your folder names?"
+read prefix
+
+for ((folder=1;folder<=number;folder++)); #initiate a for loop that loops the number of times specified in the second argument
 do
-	if [ $week -lt 10 ]; then
-		mkdir week_0$week #this adds a '0' at the beginning of the week number so as the folders are sorted numerically
+	if [ $folder -lt 10 ]; then
+		mkdir "$prefix"_0"$folder" #this adds a '0' at the beginning of the folder number so as the folders are sorted numerically
 	else
-		mkdir week_$week
+		mkdir "$prefix"_$folder
 	fi
 done
